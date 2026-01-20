3618bbaa3c95c3ece2e73b4dbdb02aacf7cfe0ca
Hariz Razin <134064848+Zeyynn@users.noreply.github.com>
2026-01-20 22:45:14 +0800

    added genre API and seperated schema to each table

--- MovieListBackend/app/Models/Movie.php
+++ MovieListBackend/app/Models/Movie.php
@@ -22,4 +22,9 @@ class Movie extends Model
     {
         return $this->belongsTo(Director::class);
     }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
 }
